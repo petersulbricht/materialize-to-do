ff982dc5c4a6bce28fa362612b2e1dcc6f193fe6
Peter S Ulbricht <37149917+petersulbricht@users.noreply.github.com>
2019-07-27 22:46:34 -0700

    App Updated
    * Removed original custom styling to use Materialize CSS
    * Cleaned up JS and added a buggy task removal function

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-let btn = document.getElementById('button');
-let input = document.getElementById('item');
+let btn = document.getElementById('btn');
+let input = document.getElementById('input');
 
-btn.addEventListener('click', addItem);
+btn.addEventListener('click', addTask);
 
-function addItem() {
-    var item = document.createElement("li");
+// add task function
+function addTask() {
+    var list = document.getElementById("list");
+    var task = document.createElement("li");
     var node = document.createTextNode(input.value);
-    item.appendChild(node);
 
-    var element = document.getElementById("items");
-    element.appendChild(item);
+    task.setAttribute('id', "task");
+    task.addEventListener('click', removeTask);
+
+    task.appendChild(node);
+    list.appendChild(task);
 
     input.value = " ";
 }
+
+
+//The removeTask function is removing tasks at random. Need to fix that...
+
+// remove task function
+function removeTask() {
+    var element = document.getElementById("task");
+    element.parentNode.removeChild(element);
+}
+
+// FUTURE FEATURE LIST:
+/*
+    saved lists (local storage)
+    complete item
+    edit item
+    move item
+    group items
+    title groups (create new groups)
+*/
